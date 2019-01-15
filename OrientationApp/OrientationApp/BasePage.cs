ae705613a452827b6fd6aed3a155d29e001c0167
AndrewBohdanovich <AndrewBohdanovich@gmail.com>
2019-01-15 13:41:17 +0300

    Add project files.

---
+++ OrientationApp/OrientationApp/BasePage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace OrientationApp
+{
+    public class BasePage : ContentPage
+    {
+        public List<VisualElement> visualElement { get; set; }
+        public BasePage()
+        {
+            DeviceDisplay.MainDisplayInfoChanged += OnMainDisplayInfoChanged;
+        }
+        void OnMainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+        {
+            var displayInfo = e.DisplayInfo.Orientation;
+            if (displayInfo == DisplayOrientation.Portrait)
+            {
+                foreach (var views in visualElement)
+                {
+                    VisualStateManager.GoToState(views, "Portrait");
+                }
+            }
+            else
+            {
+                foreach (var views in visualElement)
+                {
+                    VisualStateManager.GoToState(views, "Landscape");
+                }
+            }
+        }
+    }
+}
---
+++ OrientationApp/OrientationApp/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrientationApp
+{
+    public partial class MainPage : BasePage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            visualElement = new List<VisualElement>()
+            {
+                ImageLayout,
+                InfoLayout
+            };
+        }
+    }
+}
